42f7cee1f80d21eb8fb168decf6381b5b6d0a061
Christopher Lee <christopher.lee@tsc.com>
2020-03-04 18:52:35 -0500

    Update install_nvidia_drivers.sh

--- install_nvidia_drivers.sh
+++ install_nvidia_drivers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dnf update -y
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf install akmod-nvidia
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y akmod-nvidia
