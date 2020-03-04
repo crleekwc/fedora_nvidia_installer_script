e50c2b2003fe29eeb44a2fad63bbc1b80e9be3dc
Christopher Lee <christopher.lee@tsc.com>
2020-03-04 18:52:13 -0500

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo dnf install -y VirtualBox nmap vim ffmpeg htop
+dnf copr enable -y zirix/gdm-wallpaper
+sudo dnf install -y VirtualBox nmap vim ffmpeg htop gdm-wallpaper
+
+# Notes:
+# To change the default wallpaper on the login screen run
+# `set-gdm-wallpaper </path/to/image.png>`
