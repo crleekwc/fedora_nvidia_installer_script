1e2ed073396a8db5f790ea49a896e092505ae67b
Christopher Lee <Crleekwc@gmail.com>
2020-03-04 19:04:50 -0500

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 dnf copr enable -y zirix/gdm-wallpaper
 dnf install -y VirtualBox nmap vim ffmpeg htop gdm-wallpaper
-
-# Notes:
-# To change the default wallpaper on the login screen run
-# `set-gdm-wallpaper </path/to/image.png>`
-#
-# If gdm fails to load hit alt + F6 (single user mode?),
-# then run this command
-# `cp /usr/share/gnome-shell/gnome-shell-theme.gresource.backup /usr/share/gnome-shell/gnome-shell-theme.gresource`
-
-# Resources:
-# https://github.com/DimaZirix/fedora-gdm-wallpaper
