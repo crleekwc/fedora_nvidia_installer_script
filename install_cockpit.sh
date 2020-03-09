5a2e6b7093deaf2396a0d5777e6b833ec09fb06c
Christopher Lee <Crleekwc@gmail.com>
2020-03-09 19:15:56 -0400

    Change to use dnf and add flag to auto install

--- install_cockpit.sh
+++ install_cockpit.sh
@@ -3,7 +3,7 @@
 # Cockpit is a web based system analysis tool. 
 # Go to https://<server_name>:9090 to start using the web app.
 # Username/pass is a user on the system.
-yum install cockpit
+dnf install -y cockpit
 systemctl enable --now cockpit.socket
 firewall-cmd --add-service=cockpit --permanent
 firewall-cmd --reload
