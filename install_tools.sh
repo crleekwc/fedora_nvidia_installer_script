2f91858134c219227fffcfedcf8616c70dcdc34b
Christopher Lee <Crleekwc@gmail.com>
2020-03-11 11:30:07 -0400

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dnf copr enable -y zirix/gdm-wallpaper
-dnf install -y VirtualBox nmap vim ffmpeg htop gdm-wallpaper
+dnf install -y VirtualBox nmap vim ffmpeg htop gdm-wallpaper timeshift tuned
