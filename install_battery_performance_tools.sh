5e767f682987f4de4280a8e55da5ff70ca345d59
Christopher Lee <Crleekwc@gmail.com>
2020-03-12 13:20:32 -0400

    Update install_battery_performance_tools.sh

--- install_battery_performance_tools.sh
+++ install_battery_performance_tools.sh
@@ -4,6 +4,6 @@ dnf install -y powertop tlp tlp-rdw
 systemctl enable --now powertop.service
 systemctl enable --now tlp
 echo "Unplugged laptop and run off on battery for the powertop calibration." 
-read -n 1 -s -r -p "Press any key to continue"
+read -n 1 -s -r -p "Press any key to continue"; echo
 powertop --calibrate
 powertop --auto-tune
