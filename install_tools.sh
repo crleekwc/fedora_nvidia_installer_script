779d0b0342048c32fbbc5ec9d932e03a8fc464cd
Christopher Lee <Crleekwc@gmail.com>
2020-03-28 23:17:08 -0400

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 dnf copr enable -y zirix/gdm-wallpaper
-dnf install -y VirtualBox nmap vim ffmpeg htop gdm-wallpaper timeshift tuned
+dnf install -y VirtualBox nmap vim ffmpeg htop gdm-wallpaper timeshift tuned snapd
+ln -s /var/lib/snapd/snap /snap
+sleep 10
+snap install peco
